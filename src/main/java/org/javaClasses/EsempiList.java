5c999a438bbd44e266a8ae0883c2791fa35fc140
AleMidolo <alemidolo@gmail.com>
2022-03-15 15:56:41 +0100

    First lesson uploaded

---
+++ src/main/java/org/javaClasses/EsempiList.java
@@ -0,0 +1,92 @@
+package org.javaClasses;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.javaClasses.libreria.Book;
+
+/* 
+ * Potete trovare la documentazione relative alla classe List e ArrayList di java nei seguenti links
+ * https://docs.oracle.com/javase/8/docs/api/java/util/List.html
+ * https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
+ */
+
+public class EsempiList {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> books = new ArrayList<>();
+		
+		// inserimento di un elemento all'interno della lista
+		books.add("Il signore degli anelli");
+		books.add("Guerra e pace");
+		books.add("La Divina Commedia");
+		books.add("Il Piccolo Principe");
+		books.add("Harry Potter");
+		
+		// l'accesso a un elemento della lista 
+		System.out.println("Il terzo libro della lista �: " + books.get(2) + "\n");
+		
+		// la modifica di un elemento della lista
+		books.set(2, "Il Decamerone");
+		System.out.println("Nuova lista: " + books + "\n");
+		
+		// rimozione di un elemento
+		books.remove(2);
+		System.out.println("Rimosso il terzo elemento della lista: " + books + "\n");
+		
+		// numero di elementi presenti nella lista
+		System.out.println("Numero di elementi: " + books.size() + "\n");
+		
+		// per ciclare su una list ci sono due modi
+		
+		// 1: ciclo for primitivo
+		System.out.println("Lista dei libri con ciclo for primitivo: ");
+		for(int i=0; i<books.size(); i++) {
+			System.out.println("\t" + books.get(i));
+		}
+		System.out.println("\n");
+		
+		// 2: ciclo for avanzato
+		System.out.println("Lista dei libri con ciclo for avanzato: ");
+		for(String book : books) {
+			System.out.println("\t" + book);
+		}
+		System.out.println("\n");
+
+		// ordinare una lista
+		Collections.sort(books);
+		System.out.println("Lista ordinata crescente: \n\t" + books);
+		Collections.sort(books, Collections.reverseOrder());
+		System.out.println("Lista ordinata decrescente: \n\t" + books + "\n");
+		
+		// rimuovere tutti gli elementi della lista
+		books.clear();
+		
+		
+		ArrayList<Book> libri = new ArrayList<>();
+		libri.add(new Book("Il Signore degli Anelli", 14.5, "Fantasy"));
+		libri.add(new Book("La Divina Commedia", 10.5, "Letteratura", 5));
+		libri.add(new Book("Guerra e Pace", 12.0, "Romanzo"));
+		
+		stampa(libri);
+		
+		for(Book libro : libri) {
+			libro.applicaSconto(20);
+		}
+		
+		stampa(libri);
+		
+		libri.sort(Comparator.comparing(Book::getNome));
+		
+		stampa(libri);
+	}
+	
+	public static void stampa(ArrayList<Book> libri) {
+		for(Book libro : libri) {
+			System.out.println("Libro: " + libro.getNome() + " Prezzo: " + libro.getPrezzo());
+		}
+		System.out.println("\n");
+	}
+}
---
+++ src/main/java/org/javaClasses/EsempiString.java
@@ -0,0 +1,53 @@
+package org.javaClasses;
+
+public class EsempiString {
+	
+	/* 
+	 * Potete trovare la documentazione relative alla classe Strirng di java nel seguente link
+	 * https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sono una stringa");
+		
+		char data[] = {'a', 'b', 'c'};
+		
+		// Esempi di come inizializzare una stringa
+		String esempio = "test";
+		String alfabeto1 = new String(data);
+		
+		// metodo length() per ottenere il numero di caratteri presenti all'interno della stringa
+		// Attenzionare il backslash "\" inserito sotto, serve a fare l'escape del carattere "
+		System.out.println("Lunghezza stringa \"esempio\": " + esempio.length());
+		System.out.println("Lunghezza stringa \"collezione\": " + alfabeto1.length() + "\n");
+		
+		// concatenazione di stringhe
+		String alfabeto2 = "def";
+		String alfabeto = alfabeto1 + alfabeto2;
+		
+		System.out.println(alfabeto);
+		System.out.println("concatenazione di alfabeto1 e alfabeto2: " + alfabeto1.concat(alfabeto2));
+		System.out.println(alfabeto1 + "\n");
+		
+		// verificare se una stringa contiene un dato carattere o sottostringa
+		System.out.println("abcdef contiene la stringa abc? " + alfabeto.contains(alfabeto1));
+		System.out.println("abcdef contiene la string test? " + alfabeto.contains(esempio) + "\n");
+		
+		// verificare se due stringhe sono uguali tra di loro
+		System.out.println("La variabile alfabeto è uguale alla stringa abcdef? " + alfabeto.equals("abcdef") + "\n");
+		
+		// sostituire alcuni caratteri o sottostringhe all'interno di una stringa
+		System.out.println("Sostituisco abc con 123: " + alfabeto.replace("abc", "123"));
+		System.out.println(alfabeto + "\n");
+		
+		// estrarre una sottostringa
+		System.out.println("Estraggo dalla variabile alfabeto: " + alfabeto.substring(2));
+		System.out.println("Estraggo dall'inizio della variabile: " + alfabeto.substring(0, 2) + "\n");
+		
+		// modificare da maiuscolo a minuscolo e viceversa
+		System.out.println("Variabile alfabeto in maiuscolo: " + alfabeto.toUpperCase());
+		String maiuscolo = "ABCD";
+		System.out.println("ABCD in minuscolo: " + maiuscolo.toLowerCase());
+	}
+}
