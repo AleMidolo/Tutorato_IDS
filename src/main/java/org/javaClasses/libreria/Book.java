9da42c8b3f95b6e31125a9c900a8655cf8009255
AleMidolo <alemidolo@gmail.com>
2022-03-18 11:15:52 +0100

    Classe Book inserita

---
+++ src/main/java/org/javaClasses/libreria/Book.java
@@ -0,0 +1,95 @@
+package org.javaClasses.libreria;
+
+
+public class Book {
+
+	private String nome;
+	private String ID;
+	private Double prezzo;
+	private Integer disponibilità;
+	private String genere;
+	
+	public Book(String nome, Double prezzo, String genere){
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.genere = genere;
+		disponibilità = 1;
+		ID = generateID();
+	}
+	
+	public Book(String nome, Double prezzo, String genere, Integer disponibilità){
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.genere = genere;
+		this.disponibilità = disponibilità;
+		ID = generateID();
+	}
+	
+	private String generateID() {
+		String ID = "";
+		if(nome.length() < 4)
+			ID = nome.substring(0, nome.length());
+		else
+			ID = nome.substring(0, 4);
+		return ID;
+	}
+	
+	public String getID() {
+		return ID;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Double getPrezzo() {
+		return prezzo;
+	}
+	
+	public void setPrezzo(Double nuovoPrezzo) {
+		if(nuovoPrezzo <= 0) {
+			System.out.println("Non si può inserire un prezzo minore o uguale a 0");
+			return;
+		}
+		prezzo = nuovoPrezzo;
+	}
+	
+	public String getGenere() {
+		return genere;
+	}
+	
+	public void aggiungiDisponibilità(int disp) {
+		if(disp <= 0) {
+			System.out.println("Non si può aggiungere una disponibilità minore o uguale a 0");
+			return;
+		}
+		disponibilità += disp;
+	}
+	
+	public void sottraiDisponibilità(int disp) {
+		if(disp <= 0) {
+			System.out.println("Non si può sottrarre una disponibilità minore o uguale a 0");
+			return;
+		}
+		else if(disponibilità < disp) {
+			System.out.println("Non c è disponibilità del prodotto");
+			return;
+		}
+		
+		disponibilità -= disp;
+	}
+	
+	public Integer getDisponibilità() {
+		return disponibilità;
+	}
+	
+	public void applicaSconto(Integer percentuale) {
+		if(percentuale <= 0 || percentuale >= 100) {
+			System.out.println("Non è possibile effettuare lo sconto del " + percentuale + "%");
+			return;
+		}
+	
+		Double nuovoPrezzo = prezzo - (prezzo/100*percentuale);
+		prezzo = nuovoPrezzo;
+	}
+}
