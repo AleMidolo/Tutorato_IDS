6a2fde3792c41eff79e85e9f21173801547e1f16
AleMidolo <alemidolo@gmail.com>
2022-03-22 16:59:07 +0100

    Aggiunto materiale seconda lezione

---
+++ src/main/java/org/javaClasses/EsempiFile.java
@@ -0,0 +1,84 @@
+package org.javaClasses;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class EsempiFile {
+
+	public static void main(String[] args) {
+
+		String nomeFile = "esempio.txt";
+		File esempio = new File(nomeFile);
+		
+		System.out.println("Il file esempio.txt è presente nella cartella corrente? " + esempio.exists());
+		System.out.println("Creo il file..");
+		try {
+			esempio.createNewFile();
+		} catch (IOException e) {
+			System.out.println("Errore nella creazione del file");
+			e.printStackTrace();
+		}
+		System.out.println("Il file esempio.txt è presente nella cartella corrente? " + esempio.exists() + "\n");
+		
+		System.out.println("Posso leggere il file? " + esempio.canRead());
+		System.out.println("Posso scrivere sul file? " + esempio.canWrite() + "\n");
+		
+		try {
+			// creazione di un writer che permette di scrivere all'interno di un file
+			FileWriter writer = new FileWriter(nomeFile);
+			writer.write("Prova di scrittura su file");
+			writer.close();
+			
+			// creazione di un reader che permetter di leggere all'interno di un file
+			BufferedReader reader = Files.newBufferedReader(Paths.get(nomeFile));
+			while (true) {
+				String riga = reader.readLine(); // legge una linea dal file
+				if (riga == null)
+					break; // controlla se il file e' finito
+				System.out.println(riga);
+			}
+			reader.close();
+			System.out.println("\n");
+			
+			// rimozione del file se presente
+			esempio.delete();
+		} catch (IOException e) {
+			System.out.println("Errore nella scrittra del file " + nomeFile);
+			e.printStackTrace();
+		}
+		
+		File cartella = new File ("C:\\Users\\Ale-m\\Desktop\\Tutorato_IDS\\");
+		File nuovaCartella = new File("C:\\Users\\Ale-m\\Desktop\\Tutorato_IDS\\NuovaCartella");
+		// creo una nuova cartella il cuo percorso è quello specificato dal File su cui chiamo il metodo
+		nuovaCartella.mkdir();
+		nuovaCartella.delete();
+		
+		System.out.println("Lista dei file presenti all'interno della cartella Tutorato_IDS: ");
+		String[] files = cartella.list();
+		for(String s : files) {
+			System.out.println(s);
+		}
+		System.out.println("\n");
+		
+		String listaLibri = "C:\\Users\\Ale-m\\Desktop\\Tutorato_IDS\\resources\\Libri.csv";
+		
+		try {
+			BufferedReader reader = Files.newBufferedReader(Paths.get(listaLibri));
+			while (true) {
+				String riga = reader.readLine(); // legge una linea dal file
+				if (riga == null)
+					break; // controlla se il file e' finito
+				System.out.println(riga);
+			}
+			reader.close();
+			System.out.println("\n");
+		}
+		catch(IOException e) {
+			
+		}
+	}
+}
---
+++ src/main/java/org/javaClasses/EsempiMap.java
@@ -0,0 +1,56 @@
+package org.javaClasses;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EsempiMap {
+
+	/* 
+	 * Potete trovare la documentazione relative alla classe List e ArrayList di java nei seguenti links
+	 * https://docs.oracle.com/javase/8/docs/api/java/util/Map.html
+	 * https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
+	 */
+	public static void main(String[] args) {
+
+		HashMap<String,String> capitali = new HashMap<>();
+		
+		// inserimento di una coppia "chiave-valore"
+		capitali.put("Italia", "Catania");
+		System.out.println(capitali);
+		capitali.put("Italia", "Roma");
+		System.out.println(capitali + "\n");
+		
+		capitali.put("Germania", "Berlino");
+		capitali.put("Francia", "Parigi");
+		capitali.put("Spagna", "Madrid");
+		
+		System.out.println("La capitale della Germania è: " + capitali.get("Germania") + "\n");
+		
+		// rimozione delle coppia associata alla chiave passata come parametro
+		capitali.remove("Germania");
+		System.out.println(capitali + "\n");
+		
+		// insieme delle chiavi
+		Set<String> chiavi = capitali.keySet();
+		// insieme dei valori
+		HashSet<String> valori = new HashSet<>(capitali.values());
+		
+		System.out.println("Insieme delle chiavi: " + chiavi);
+		System.out.println("Insieme dei valori: " + valori + "\n");
+		
+		for(String chiave : chiavi) {
+			System.out.println("Nazione: " + chiave);
+		}
+		System.out.println("\n");
+		
+		for(String valore : valori) {
+			System.out.println("Capitale: " + valore);
+		}
+		System.out.println("\n");
+		
+		for(String chiave : chiavi) {
+			System.out.println("Nazione: " + chiave + ", Capitale: " + capitali.get(chiave));
+		}
+		System.out.println("\n");
+	}
+}
